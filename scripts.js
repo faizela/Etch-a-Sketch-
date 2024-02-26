4f2a1732e12824e04814aedc2a6af1c8e973aa49
Feisal Ahmed <feisala132@gmail.com>
2024-02-26 10:48:32 +0000

    Adding btnclicked function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
 const container = document.getElementById('container')
+const btn = document.getElementById('btn')
 
+//
 for (let i = 0; i < 256;  i++ ){
     const div = document.createElement("div")
     div.classList.add('divs')
     container.appendChild(div)
 }
+
+function btnClicked() {
+    let squares = prompt("Enter number of squares per side ")
+    while (squares > 100) {
+        squares =  prompt("Enter number of squares per side no more than 100")
+    }
+    container.innerHTML = ''
+    const totalsquares = squares * squares
+
+    for (let i = 0; i < totalsquares; i++) {
+        const div = document.createElement("div")
+        div.classList.add('divs')
+        container.appendChild(div)
+    }
+
+    const divs = Array.from(document.getElementsByClassName('divs'))
+
+    divs.forEach(div => div.addEventListener('mouseover', function(e) {
+            e.target.classList.add('black')
+            }))
+}
+
+btn.addEventListener('click', btnClicked)
+
+const divs = Array.from(document.getElementsByClassName('divs'))
+
+
+
+ 
+divs.forEach(div => div.addEventListener('mouseover', function(e) {
+e.target.classList.add('black')
+}))
+
+
+
+    
